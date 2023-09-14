05dc1cfa824754811a7c5988dfce5801175a5e5d
Wakana <wakanichi13@gmail.com>
2023-09-14 17:36:10 +0400

    feat: final commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min.js";
 import App from "./App.jsx";
 import "./index.css";
+import { FavoritesContextProvider } from "./store/favorites-context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </FavoritesContextProvider>
 );
